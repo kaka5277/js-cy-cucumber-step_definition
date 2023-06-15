22bc19617ffbbcf8726f704657e246eee36d288f
“kaka5277” <254140626@qq.com>
2023-06-15 14:15:22 +1000

    step目录

---
+++ cypress/e2e/newtoursp/newtours1.js
@@ -0,0 +1,29 @@
+import { Before, After, Given, When, Then, And } from "cypress-cucumber-preprocessor/steps";
+
+Before(()=>{
+    cy.reload()
+})
+
+After(()=>{
+    cy.reload()
+})
+
+Given("open newtours application", () => {
+    cy.visit("https://demo.guru99.com/test/newtours")
+    
+})
+
+When("I log in as Following", (datatable) => {
+    datatable.hashes().forEach(element => {
+        cy.get('[name=userName]').type(element.userName)
+        cy.get('[name=password]').type(element.password)
+    })
+})
+
+Then("click on submit button", () => {
+    cy.get('[name=submit]').click()
+})
+
+And("verify title should be {string}", (title) => {
+    cy.title().should('eq', title)
+})
---
+++ readme.md
@@ -0,0 +1,7 @@
+Cypress with Cucumber
+https://www.youtube.com/watch?v=TGLQN3xEnaU&list=PLQKDzuA2cCjrHFelJTVP60NPcVpdGnWHS&index=6
+js 
+多个.feature，调用多个js文件
+运行后，显示e2e下2个.feature
+ 随便一个目录:.ts文件夹,.feature必须放一起
+  "step_definitions": "cypress/e2e" ,必须写个目录，即使不放这里
